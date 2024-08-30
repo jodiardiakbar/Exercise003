31776bfb746b28a2ae3eb54c43e3c730b33ad5f3
jodiardiakbar <jodiardi.akbar@gmail.com>
2024-08-30 11:00:31 -0400

    feat: modify exercise003.ts

--- exercise003.ts
+++ exercise003.ts
@@ -1,1 +1,14 @@
-// baru upload file kosong
+// 1. Character case swap
+let originalCharacter: string = `The QuiCk BrOwN Fox`;
+let swapCaseCharacter: string = ``;
+let i: number = 0;
+while (i < originalCharacter.length) {
+  console.log(i);
+  if (originalCharacter[i] === originalCharacter[i].toLowerCase()) {
+    swapCaseCharacter += originalCharacter[i].toUpperCase();
+  } else {
+    swapCaseCharacter += originalCharacter[i].toLocaleLowerCase();
+  }
+  i++;
+}
+console.log(`${originalCharacter} -> ${swapCaseCharacter}`);
