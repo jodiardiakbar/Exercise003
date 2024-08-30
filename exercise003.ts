65c49d04e8184dc487b110b386c792f103188498
jodiardiakbar <jodiardi.akbar@gmail.com>
2024-08-30 16:21:24 -0400

    feat: modify exercise003.ts

--- exercise003.ts
+++ exercise003.ts
@@ -13,7 +13,7 @@ while (i < originalCharacter.length) {
 }
 console.log(`${originalCharacter} -> ${swapCaseCharacter}`);
 
-// 2. largest integers
+// 2. find the largest of two given integers
 let num1: number = 42;
 let num2: number = 27;
 if (num1 > num2) {
@@ -21,3 +21,22 @@ if (num1 > num2) {
 } else {
   console.log(num2);
 }
+
+// 3. sort three numbers
+let num3: number = 42;
+let num4: number = 27;
+let num5: number = 18;
+
+// 4. input checker
+let _input = 100;
+if (typeof _input === `number`) {
+  console.log(`${_input} -> 2`);
+} else if (typeof _input === `string`) {
+  console.log(`${_input} -> 1`);
+} else {
+  console.log(`${_input} -> 3`);
+}
+
+// 5. change every letter a into *
+let _inputString: string = `An apple a day keeps the doctor away`;
+console.log(_inputString.replace(/[A,a]/g, `*`));
